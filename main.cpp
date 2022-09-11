78791b823be47ba49744209e19ff154ba06e3bdb
Félix Jiménez Calvo <felixjcalvo@gmail.com>
2022-09-11 12:16:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,35 +7,53 @@ int square(unsigned int i) {
 
 int main() {
     // https://stackoverflow.com/questions/11004273/what-is-stdpromise/12335206#12335206
+    // https://en.cppreference.com/w/cpp/thread/future <-- Include exception handling
+    // https://cplusplus.com/reference/future/promise
 
-    // * ASYNC
-    // Deals with creating a thread and creating a future for us
-    // std::async returns a std::future that holds the return value that will be calculated by the function
-    // std::async works without a launch policy, so std::async(square, 5); compiles.
-    // When you do that the system gets to decide if it wants to create a thread or not.
-    // The idea was that the system chooses to make a thread unless it is already running more threads than it can run efficiently.
-    // override that behavior with std::launch::async which forces the system to create a thread
-    auto f = std::async(std::launch::async, square, 8); // forcing a thread
-    std::cout << "square currently running\n"; // do something method is running
-    std::cout << "result is " << f.get() << '\n'; // getting the result
+    // * ASYNC **************************************************
+    // The most convenient and straight-forward way to perform an asynchronous computation is 
+    // via the async function template, which returns the matching future immediately:
+    int result = std::async(foo, 1.5, 'x', false); // you can use auto instead of int
+    
+    // * FUTURE **************************************************
+    // provides a mechanism to access the result of asynchronous operations.
+    // An asynchronous operation (created via std::async, std::packaged_task, or std::promise) 
+    // can provide a std::future object to the creator of that asynchronous operation
+    // future from a packaged_task
+    std::packaged_task<int()> task([]{ return 7; }); // wrap the function
+    std::future<int> f1 = task.get_future();  // get a future
+    std::thread t(std::move(task)); // launch on a thread
+ 
+    // future from an async()
+    std::future<int> f2 = std::async(std::launch::async, []{ return 8; });
+ 
+    // future from a promise
+    std::promise<int> p;
+    std::future<int> f3 = p.get_future();
+    std::thread( [&p]{ p.set_value_at_thread_exit(9); }).detach();
+ 
+    std::cout << "Waiting..." << std::flush;
+    f1.wait();
+    f2.wait();
+    f3.wait();
+    std::cout << "Done!\nResults are: " << f1.get() << ' ' << f2.get() << ' ' << f3.get() << '\n';
+    t.join();
+    
+    
+    // * PROMISE **************************************************
+    // A promise is an object that can store a value of type T to be retrieved by a future object 
+    // (possibly in another thread), offering a synchronization point.
+    void print_int (std::future<int>& fut) {
+        int x = fut.get();
+        std::cout << "value: " << x << '\n';
+    }
+    
+    std::promise<int> prom; 
+    std::future<int> fut = prom.get_future();    // engagement with future
+    std::thread th1 (print_int, std::ref(fut));  // send future to new thread
+    prom.set_value (10);                         // fulfill promise
+    th1.join();                                  // (synchronizes with getting the future)
 
-    // * FUTURE (yes, they look practically equal)
-    // A future is an object that can retrieve a value from some provider object or function,
-    // Valid" futures are future objects associated to a shared state, and are constructed by calling one of the following functions:
-    // async
-    // promise::get_future
-    // packaged_task::get_future
-    std::future<int> fut = std::async (square, 444444443); // creating our own future
-    std::cout << "isPrime currently running\n"; // do something method is running
-    std::cout << "result is " << fut.get() << '\n';
-
-    // * FUTURE AND PROMISE
-    // A PROMISE allows us to return state from a thread.
-    // Whereas a FUTURE is for reading that returned state.
-    std::promise<int> promise;
-    std::future<int> future = promise.get_future();
-    promise.set_value(square(5));
-    std::cout << "result is " << future.get() << '\n';
-
+    
     return EXIT_SUCCESS;
 }
